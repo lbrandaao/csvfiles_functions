de54ceb9ec58dd4ac4d9818f5aa20abd0b8b8bbe
Leonardo Brandão <leonardobbrandao@hotmail.com>
2020-07-04 20:15:08 -0300

    Modo Excel

--- functions/conversor_csv.py
+++ functions/conversor_csv.py
@@ -5,10 +5,16 @@ def conversor_csv(arquivo, novo_arquivo, modo):
     file_read = open(arquivo, 'r')
     file_write = open(novo_arquivo, 'w')
     
-    '''A finalidade do modo 'pandas' é converter um arquivo csv com separador ';', com normalização NFC e com números fracionários com ',' -> para separador ',', remover acentos e números fracionários com '.'. Assim o pandas é capaz de ler.'''
+    '''A finalidade do modo 'pandas' é converter um arquivo csv com separador ';', com normalização NFC e com números fracionários com ',' -> para separador ',', substituir caracteres especiais e deixar números fracionários com '.'. Assim o pandas é capaz de ler.'''
     if modo == 'pandas':
         for line in file_read:
             nfkd = unicodedata.normalize('NFKD', line) #Normalização da linha na forma NKFD.
             semacento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
             new_line = semacento.replace(',','.') #.replace(';', ',') caso não use o parâmetro (sep=',') em read_csv(). 
+            file_write.write(new_line)
+    
+    '''A finalidade do modo 'excel' é converter um arquivo csv com separador ',' -> para separador ';'. Dessa maneira os dados se distribuem corretamente nas células do Excel.'''
+    if modo == 'excel':
+        for line in file_read:
+            new_line = line.replace(',', ';')
             file_write.write(new_line)
