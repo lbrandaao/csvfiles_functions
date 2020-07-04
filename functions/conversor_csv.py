1a193329109206e67dce9315fc5c1c82462e9063
Leonardo Brandão <leonardobbrandao@hotmail.com>
2020-07-04 18:38:44 -0300

    Ajuste comentário

--- functions/conversor_csv.py
+++ functions/conversor_csv.py
@@ -5,7 +5,7 @@ def conversor_csv(arquivo, novo_arquivo, modo):
     file_read = open(arquivo, 'r')
     file_write = open(novo_arquivo, 'w')
     
-    '''A finalidade do modo 'pandas' é converter um arquivo csv com separador ';' e com normalização NFC, para o separador ',' e remover acentos. Assim o pandas é capaz de ler.'''
+    '''A finalidade do modo 'pandas' é converter um arquivo csv com separador ';', com normalização NFC e com números fracionários com ',' -> para separador ',', remover acentos e números fracionários com '.'. Assim o pandas é capaz de ler.'''
     if modo == 'pandas':
         for line in file_read:
             nfkd = unicodedata.normalize('NFKD', line) #Normalização da linha na forma NKFD.
