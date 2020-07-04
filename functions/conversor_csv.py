8790a2255bdef2c468fa1093ae3d76e50b854448
Leonardo Brandão <leonardobbrandao@hotmail.com>
2020-07-04 18:03:59 -0300

    Modo Pandas
    convertendo para uma versão que o pandas seva capaz de ler.

--- functions/conversor_csv.py
+++ functions/conversor_csv.py
@@ -1,12 +1,14 @@
 import unicodedata
 import pandas as pd
 
-def conversor_excel(arquivo, novo_arquivo, modo):
-    if modo == 'pycharm':
-        file_read = open(f'C:\\Users\\leona\\PycharmProjects\\CursoDados\\arquivos_excel\\{arquivo}', 'r')
-        file_write = open(f'C:\\Users\\leona\\PycharmProjects\\CursoDados\\arquivos_excel\\{novo_arquivo}', 'w')
+def conversor_csv(arquivo, novo_arquivo, modo):
+    file_read = open(arquivo, 'r')
+    file_write = open(novo_arquivo, 'w')
+    
+    '''A finalidade do modo 'pandas' é converter um arquivo csv com separador ';' e com normalização NFC, para o separador ',' e remover acentos. Assim o pandas é capaz de ler.'''
+    if modo == 'pandas':
         for line in file_read:
-            nfkd = unicodedata.normalize('NFKD', line)
+            nfkd = unicodedata.normalize('NFKD', line) #Normalização da linha na forma NKFD.
             semacento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
-            new_line = semacento.replace(',','.').replace(';', ',')
+            new_line = semacento.replace(',','.') #.replace(';', ',') caso não use o parâmetro (sep=','). 
             file_write.write(new_line)
