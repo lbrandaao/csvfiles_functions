c8d9f34a5d7dacdb4a63be759021a8406181e64a
Leonardo Brandão <leonardobbrandao@hotmail.com>
2020-07-05 23:20:29 -0300

    Update conversor_csv.py
    Agora é possível selecionar os separadores e não ficar restrito aos modos. Transformar um arquivo .txt em .csv.

--- functions/conversor_csv.py
+++ functions/conversor_csv.py
@@ -19,12 +19,15 @@ def conversor_csv(arquivo, novo_arquivo, sepor, sepnov, caracesp = False):
             equi_line = u"".join([c for c in nfkd if not unicodedata.combining(c)])
             file_write.write(equi_line)
     
+    elif sepor == ',':
+        for line in file_read:
+            line = line.replace(sepor, sepnov)
+            file_write.write(line)
     else:
-        if sepor == ','
-            for line in file_read:
-                line = line.replace(sepor, sepnov)
-                file_write.write(line)
-        else:
-            for line in file_read:
-                line = line.replace(',', '.').replace(sepor, sepnov)
-                file_write.write(line)
+        for line in file_read:
+            line = line.replace(',', '.').replace(sepor, sepnov)
+            file_write.write(line)
+
+#Em alguns casos, dados numéricos decimais podem possuir ',' para indicar os algarismos decimais. Caso o separador das colunas também seja ',' é impossível diferir o indicador de casas decimais e o separador de coluna (sempre que o indicador e o separador forem a mesma string isso ocorre). Portanto, caso o separador original não seja ',', a função sempre irá substituir o indicador de casas decimais ',' por '.'.
+
+#Necessário aprimorar para o caso de dados separados por '.' e números decimais com ','.
