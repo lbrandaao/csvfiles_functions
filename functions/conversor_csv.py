e1ebdc6891ef882c2100ab298d826d55e4a615f1
Leonardo Brandão <leonardobbrandao@hotmail.com>
2020-07-11 21:52:51 -0300

    Update conversor_csv.py

--- functions/conversor_csv.py
+++ functions/conversor_csv.py
@@ -26,55 +26,16 @@ def conversor_csv(file, new_file, orsep, newsep, ordecsep = '.', spechar = True,
         file_write.write(equi_content)
 
     else:
+        reading = file_read.read()
+        new_content = reading.replace(ordecsep, '.').replace(orsep, newsep)
+        nfd = unicodedata.normalize('NFD', new_content)
+        equi_content = u"".join([c for c in nfd if not unicodedata.combining(c)])
+        file_write.write(equi_content)
 
+    file_write.close()
+    file_read.close()
 
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    '''if caracesp == False and sepor == ',':
-        for line in file_read:
-            line = line.replace(sepor, sepnov)
-            nfd = unicodedata.normalize('NFD', line)
-            equi_line = u"".join([c for c in nfd if not unicodedata.combining(c)])
-            file_write.write(equi_line)
-    
-    elif caracesp == False:
-        for line in file_read:
-            line = line.replace(',', '.').replace(sepor, sepnov)
-            nfd = unicodedata.normalize('NFD', line)
-            equi_line = u"".join([c for c in nfd if not unicodedata.combining(c)])
-            file_write.write(equi_line)
-    
-    elif sepor == ',':
-        for line in file_read:
-            line = line.replace(sepor, sepnov)
-            file_write.write(line)
-    else:
-        for line in file_read:
-            line = line.replace(',', '.').replace(sepor, sepnov)
-            file_write.write(line)'''
 
 #Em alguns casos, dados numéricos decimais podem possuir ',' para indicar os algarismos decimais. Caso o separador das colunas também seja ',' é impossível diferir o indicador de casas decimais e o separador de coluna (sempre que o indicador e o separador forem a mesma string isso ocorre). Portanto, caso o separador original não seja ',', a função sempre irá substituir o indicador de casas decimais ',' por '.'.
 
-#Necessário aprimorar para o caso de dados separados por '.' e números decimais com ','.
+#Necessário aprimorar para o caso de dados separados por strings diferentes de ',' ou ';' e com números decimais com ','.
