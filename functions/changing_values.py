aabfc271d9fcdcb068bf82cb212f45f5b30b93f5
Leonardo Brandão <leonardobbrandao@hotmail.com>
2020-07-18 18:21:18 -0300

    Update changing_values.py

--- functions/changing_values.py
+++ functions/changing_values.py
@@ -1,4 +1,13 @@
-import pandas as pd 
+ def changing_values(file, new_file, perc, rows, columns, sep = ',', orencoding = 'utf-8', newencoding = 'utf-8'):
+     orfile = open(file, 'r', encoding = orencoding)
+     newfile = open(new_file, 'w', encoding = newencoding)
 
-def changing_values(file):
-    df = pd.read_csv(file)
+     rows_count = 2
+     for line in orfile:
+         columns_count = 1
+         if rows_count in rows:
+            for word in line:
+                if word == sep:
+
+
+
